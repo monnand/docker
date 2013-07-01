e89ae8fbcb02c4a4b7867028f37d17c389a9f40f
Nan Monnand Deng <monnand@gmail.com>
2013-07-01 19:05:05 -0400

    fixed #910. print user name to docker info output

--- commands.go
+++ commands.go
@@ -467,6 +467,11 @@ func (cli *DockerCli) CmdInfo(args ...string) error {
 		fmt.Fprintf(cli.out, "Fds: %d\n", out.NFd)
 		fmt.Fprintf(cli.out, "Goroutines: %d\n", out.NGoroutines)
 	}
+	if cli.authConfig != nil {
+		fmt.Fprintf(cli.out, "Username: %v\n", cli.authConfig.Username)
+		// XXX Should we print registry address even if the user was not logged in?
+		fmt.Fprintf(cli.out, "Registry: %v\n", auth.IndexServerAddress())
+	}
 	if !out.MemoryLimit {
 		fmt.Fprintf(cli.err, "WARNING: No memory limit support\n")
 	}
